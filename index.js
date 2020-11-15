67bd9dc305179ea1dc6b1361f73adc210525f1b7
boyloe <boyloe@gmail.com>
2020-11-15 12:01:22 -0700

    Build api

---
+++ index.js
@@ -0,0 +1,26 @@
+import axios from "axios";
+
+const public_token = 'pk_ae113656c0fe4cbb8f2053e075fa29b8' 
+const api = axios.create({
+    baseURL: "https://cloud.iexapis.com/stable",
+});
+
+export const loadQuotesForStock = symbol => {
+
+    return api.get(`/stock/${symbol}/quote?token=${public_token}`).then(res => res.data);
+};
+
+export const loadLogoForStock = symbol => {
+
+    return api.get(`/stock/${symbol}/logo?token=${public_token}`).then(res => res.data.url);
+};
+
+export const loadRecentNewsForStock = symbol => {
+
+    return api.get(`/stock/${symbol}/news?token=${public_token}`).then(res => res.data);
+};
+
+export const loadChartForStock = (symbol, range) => {
+
+    return api.get(`/stock/${symbol}/chart/${range}?token=${public_token}`).then(res => res.data);
+};
